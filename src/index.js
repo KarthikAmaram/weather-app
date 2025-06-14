aef540d553d427114a8c12c562705e99b6281313
Karthik Amaram <amaramkart09@gmail.com>
2025-06-14 17:34:52 -0500

    Finished the project :D

--- src/index.js
+++ src/index.js
@@ -1,23 +1,52 @@
 import "./styles.css";
 
-async function getWeatherData() {
+const locationInput = document.getElementById("locationInput");
+const fahrenheitBtn = document.getElementById("fahrenheit");
+const celsiusBtn = document.getElementById("celsius");
+const weatherBtn = document.getElementById("weatherButton");
+const form = document.querySelector("form");
+let unit = "fahrenheit";
+
+async function getWeatherData(location, unit) {
     const img = document.querySelector("img");
-    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/la?key=DB7GXJD6FSKZ3UFSG8E4HKK7B", {mode: "cors"})
+    const conditions = document.querySelector(".conditions");
+    const temp = document.querySelector(".temp");
+    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ location + "?key=DB7GXJD6FSKZ3UFSG8E4HKK7B", {mode: "cors"})
     const weatherData = await response.json();
     let precipate = weatherData.currentConditions.preciptype;
-    console.log(weatherData.currentConditions.temp);
-    console.log(weatherData);
+    conditions.textContent = `Description: ${weatherData.description}`;
+    if (unit === "fahrenheit") {
+        temp.textContent = `Temperature in Fahrenheit: ${weatherData.currentConditions.temp}`;
+    } else {
+        let temperature = parseInt(weatherData.currentConditions.temp)
+        temperature = (temperature - 32) * (5/9);
+        temp.textContent = `Temperature in Celsius: ${Math.round(temperature * 10) / 10}`;
+    }
     if (precipate == null) {
-        precipate = "boring";
+        precipate = "nothing";
     }
     fetch('https://api.giphy.com/v1/gifs/translate?api_key=V2jTsSqkbRj9JiBk8EQXy6uEL1hKnRnA&s=' + precipate, {mode: 'cors'})
         .then((response) => {
             return response.json();
         })
         .then((response) => {
-            // img.src = response.data.images.original.url;
-            console.log(response.data.images.original.url);
+            img.src = response.data.images.fixed_height.url;
         })
 }
 
-// getWeatherData();
+fahrenheitBtn.addEventListener("click", () => {
+    unit = "fahrenheit";
+});
+
+celsiusBtn.addEventListener("click", () => {
+    unit = "celsius";
+})
+
+weatherBtn.addEventListener("click", () => {
+    const location = locationInput.value.toLowerCase();
+    getWeatherData(location, unit);
+})
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+})
